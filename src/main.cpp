37521c56f741d974d8401fa85bc0af520855178b
glaentzlin kevin <kevin1.glaentzlin@epitech.eu>
2024-05-18 01:25:42 +0200

    [+] restore game loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,15 @@
 ** EPITECH PROJECT, 2024
 ** RIP-JO
 ** File description:
-** main.cpp
+** main
 */
 
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include "RIPJO/Parsing/Parsing.hh"
-#include "RIPJO/Player/Player.hh"
-#include "RIPJO/Time/Time.hh"
-#include "unistd.h"
+#include <raylib.h>
 
-int main() {
-    RIPJO::Player player;
-    RIPJO::Time time(player);
-    RIPJO::Parsing parser;
+#include "RIPJO/RIPJO.hh"
 
-    // std::this_thread::sleep_for(std::chrono::seconds(5));
-    // std::cout << "Player influence after 5 seconds: " << player.getInfluence() << std::endl;
-    parser.initialize("config/event.format");
-    parser.parseDistrict("Champs-mars");
-    parser.parseDistrict("Champs-elysee");
+int main(void)
+{
+    RIPJO::RIPJO game;
     return 0;
 }
