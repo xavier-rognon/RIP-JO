8041642365dcd86964e405f6dcca546636d34f5b
Arthur Spiller <arthur.spiller@epitech.eu>
2024-05-17 22:44:40 +0200

    [+] add base of Incident class

--- src/RIPJO/Incident/AIncident.hh
+++ src/RIPJO/Incident/AIncident.hh
@@ -11,14 +11,16 @@
 namespace RIPJO {
 
     class AIncident : public IIncident {
+    public:
+        AIncident(std::string name, std::vector<std::size_t> targets,
+                  std::size_t influenceCost, std::size_t unrestGain);
+        ~AIncident() = default;
 
-        public:
-            AIncident();
-            ~AIncident() = default;
-
-        protected:
-
-        private:
-
+        
+    protected:
+        std::string _name;
+        std::vector<std::size_t> _targetDistricts;
+        std::size_t _influenceCost;
+        std::size_t _unrestGain;
     };
 }
